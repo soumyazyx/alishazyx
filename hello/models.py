ff5e98137e5c767cd24b3a90452084e75e947880
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-07-12 17:19:52 +0530

    Added foreign key relnship

--- hello/models.py
+++ hello/models.py
@@ -4,10 +4,10 @@ from django.utils import timezone
 
 class Category(models.Model):
     # id = models.AutoField(primary_key=True) - added automatically
-    category_name = models.CharField(max_length=255, blank=False)
-    category_desc = models.TextField(blank=True)
-    category_created_on = models.DateTimeField(auto_now_add=True)
-    category_last_modified = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=False)
+    desc = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name_plural = "categories"
@@ -19,6 +19,7 @@ class Category(models.Model):
 class Product(models.Model):
     product_sku = models.CharField(max_length=255, blank=True, default="-1")
     product_title = models.CharField(max_length=255, blank=False)
+    product_category = models.ForeignKey(Category, on_delete=models.CASCADE)
     product_desription = models.TextField(blank=True)
     product_image = models.ImageField(blank=True, upload_to="images/")
 
@@ -26,4 +27,4 @@ class Product(models.Model):
         verbose_name_plural = "Products"
 
     def __str__(self):
-        return "{} - {}".format()
+        return "{} - {}".format(self.product_sku, self.product_title)
