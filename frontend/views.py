aeedbfdc68f879c400f532ff86af06300f03d9c4
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-07-18 01:52:34 +0530

    added products page

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,5 @@
 import json
-
+from django.http import HttpResponse
 from django.core import serializers
 from django.shortcuts import render, get_object_or_404
 
@@ -14,17 +14,13 @@ def index(request):
 
 
 def product_view(request, categoryname):
-    # get category id from category name
-    category_qs = Category.objects.filter(name__iexact=categoryname)
-    # Check if we have any product/s for the given category
-    if len(category_qs) > 0:
-        category_qs_json = json.loads(serializers.serialize("json", category_qs))
-        category_id = category_qs_json[0]["pk"]
-        # Filter products based on category id
-        products = Product.objects.filter(product_category_id=category_id)
-        return render(request, "products.html", {"products": products})
-    else:
-        return render(request, "products.html")
+    category = get_object_or_404(Category, name=categoryname)
+    products = Product.objects.filter(category=category)
+    return render(
+        request,
+        "frontend/products.html",
+        {"products": products, "category": categoryname},
+    )
 
 
 def detail_view(request, categoryname, id):
--- hello/models.py
+++ hello/models.py
@@ -17,26 +17,24 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    product_sku = models.CharField(max_length=255, blank=True, default="-1")
-    product_title = models.CharField(max_length=255, blank=False, unique=True)
-    product_category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    product_desription = models.CharField(max_length=255, blank=True)
-    product_image = models.ImageField(blank=True, upload_to="images/")
-    product_created_on = models.DateTimeField(auto_now_add=True)
-    product_last_modified = models.DateTimeField(auto_now=True)
+    sku = models.CharField(max_length=255, blank=True, default="-1")
+    title = models.CharField(max_length=255, blank=False, unique=True)
+    image = models.ImageField(blank=True, upload_to="images/products/")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    desription = models.CharField(max_length=255, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name_plural = "Products"
 
     def __str__(self):
-        return "{} - {} - {}".format(
-            self.product_category, self.product_sku, self.product_title
-        )
+        return "{} - {}".format(self.category, self.title)
 
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(upload_to="images/products/")
 
     def __str__(self):
         return self.product.product_title
