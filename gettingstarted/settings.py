3552dd65f6d1c09722d4f5b26ec3613f1a1f19a7
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-07-19 02:50:46 +0530

    .

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "#t79p^gzq(t13jj-3uec1_wu13%uzkyvlz!9cab@8njhe=8@m6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = []
 
