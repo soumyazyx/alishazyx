528b80b3b8ce1aafbd43fdba621124180765db2d
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-07-23 02:14:19 +0530

    twilio - with post

--- bot/views.py
+++ bot/views.py
@@ -4,6 +4,13 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 @twilio_view
 def dummy(request):
+    # r = MessagingResponse()
+    # r.message("Thanks for the SMS message!")
+    # return r
+
+    name = request.POST.get("Body", "")
+    msg = "Hey %s, how are you today?" % (name)
     r = MessagingResponse()
-    r.message("Thanks for the SMS message!")
+    r.message(msg)
     return r
+
