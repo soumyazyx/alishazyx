4b5dc5aa04c732a5f13759a299673367ac3c70e6
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-07-23 01:12:58 +0530

    added csrf_exempt

--- bot/views.py
+++ bot/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def dummy(request):
     return HttpResponse("hi there!")
