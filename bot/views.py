16f9e06db34df5c90e1f2a9aadd2cba52e387321
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-07-23 01:46:34 +0530

    added twilio method of invocation

--- bot/views.py
+++ bot/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+
+from twilio.twiml.messaging_response import MessagingResponse
 
 
 @csrf_exempt
 def dummy(request):
+    r = MessagingResponse()
+    r.message("Hello from your Django app!")
+    return HttpResponse(r.toxml(), content_type="text/xml")
+
+
+def dummy2(request):
     twiml = "<Response><Message>Hello from your Django app!</Message></Response>"
     return HttpResponse(twiml, content_type="text/xml")
