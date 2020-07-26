0a77b95eb29e480f7dc6292fa6f02e806684538d
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-07-26 21:49:49 +0530

    Integration of telegram

--- bot/views.py
+++ bot/views.py
@@ -9,7 +9,12 @@ from django.core.files import File
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django_twilio.decorators import twilio_view
-from lib.bot.bot_utils import save_telegram_message, scan_messages
+from lib.bot.bot_utils import (
+    save_telegram_message,
+    scan_messages,
+    send_message,
+    get_categories,
+)
 from rest_framework import status
 from rest_framework.response import Response
 from twilio.twiml.messaging_response import MessagingResponse
@@ -40,14 +45,15 @@ def telegram_handler(request):
     message = request.body.decode("utf-8")
     body_json = json.loads(message)
     update_id = body_json["update_id"]
+    chat_id = body_json["message"]["chat"]["id"]
+    # send_message(chat_id, "hi")
     if "text" in body_json["message"]:
         text = body_json["message"]["text"]
     else:
         text = ""
     print("Saving message to DB..")
     save_telegram_message_res = save_telegram_message(
-        json_msg=message,
-        update_id=update_id
+        json_msg=message, update_id=update_id
     )
     if save_telegram_message_res == -1:
         print("Saving message to DB..FAILED!")
@@ -56,9 +62,11 @@ def telegram_handler(request):
         print("Saving message to DB..Done [id={}]".format(
             save_telegram_message_res))
 
-        if (text.lower() == "endzyx"):
-            scan_messages()
-        return HttpResponse("ok", status=200)
+    if text.lower() == "startzyx":
+        send_message(chat_id, get_categories())
+    elif text.lower() == "endzyx":
+        scan_messages()
+    return HttpResponse("ok", status=200)
 
 
 @csrf_exempt
--- lib/bot/bot_utils.py
+++ lib/bot/bot_utils.py
@@ -1,3 +1,4 @@
+import os
 import json
 import re
 import urllib.request
@@ -8,9 +9,12 @@ from django.core.files import File
 from bot.models import TelegramMessage
 from hello.models import Product, Category, ProductImage
 
+end_marker = "endzyx"
+start_marker = "startzyx"
 base_url = "https://api.telegram.org/"
-bot_token = "1220904092:AAFa_l0w-ycsnldjLpk_noalgGRk0g0PMJo"
+bot_token = os.environ["BOT_TOKEN"]
 default_photo_url = "https://res.cloudinary.com/hxjbk5wno/image/upload/v1594845942/logo_alisha_min_vra4dr.png"
+default_category_id = 17  # Category 'Whatever' has id=17
 
 
 def save_telegram_message(update_id, json_msg):
@@ -34,164 +38,142 @@ def scan_messages():
     all_blocks = split_msges_to_blocks(telegram_msges_qs)
     print("Analysing messages to find potential products..Done")
 
-    # print("Creating new products..")
-    # save_products(all_blocks)
-    # print("Creating new products..Done")
+    print("Computing title and desc for all products..")
+    all_blocks = compute_title_desc(all_blocks)
+    print("Computing title and desc for all products..Done")
+
+    print("Check and remove existing products from list..")
+    filtered_blocks = remove_existing_products(all_blocks)
+    print("Check and remove existing products from list..Done")
+
+    print("Creating new products..")
+    save_products(filtered_blocks)
+    print("Creating new products..Done")
 
 
 def split_msges_to_blocks(telegram_msges_qs):
-    '''Takes in ALL the messages and splits into blocks
+    """Takes in ALL the messages and splits into blocks
     Each block starts with 'startzyx' and ends with 'endzyx' 
     startzyx & endzyx behave as markers here
-    '''
-    start_marker = 'startzyx'
-    end_marker = 'endzyx'
-    block_in_progress = False
-    each_block = []
+    """
+
     all_blocks = []
-    beep = []
-    block = {}
-    counter = 1
-    block = {}
-    block['text'] = []
-    block['desc'] = ''
-    block['title'] = ''
-    block["photos"] = []
+    block_in_progress = False
 
     for telegram_record in telegram_msges_qs:
-
-        line = ""
         message = json.loads(telegram_record.json_msg)
-
         if "text" in message["message"]:
             line = message["message"]["text"]
             if line.strip().lower() == start_marker:
                 block_in_progress = True
                 block = {}
-                block['text'] = []
-                block['desc'] = ''
-                block['title'] = ''
+                block["text"] = []
+                block["desc"] = ""
+                block["title"] = ""
                 block["photos"] = []
             elif line.strip().lower() == end_marker and block_in_progress:
                 block_in_progress = False
-                beep.append(block)
+                all_blocks.append(block)
                 block = {}
-                block['text'] = []
-                block['desc'] = ''
-                block['title'] = ''
+                block["text"] = []
+                block["desc"] = ""
+                block["title"] = ""
                 block["photos"] = []
-
             elif block_in_progress:
                 block["text"].append(line)
-        elif ("photo" in message["message"] and block_in_progress):
-            line = get_photo_id(message)
-            block["photos"].append(line)
-        elif ("animation" in message["message"] and block_in_progress):
+        elif "photo" in message["message"] and block_in_progress:
+            block["photos"].append(get_photo_id(message))
+        elif "animation" in message["message"] and block_in_progress:
             pass  # Not supported
-        elif ("video" in message["message"] and block_in_progress):
+        elif "video" in message["message"] and block_in_progress:
             pass  # Not supported
-        # elif "text" in message["message"]
-        # line = message["message"]["text"]
-        # block["text"].append(line)
 
-        # # Split lines into chunks depending upon startzyx and endzyx
-        # if line.strip().lower() == start_marker:
-        #     block_in_progress = True
-        # elif line.strip().lower() == end_marker and block_in_progress:
-        #     block_in_progress = False
-        #     all_blocks.append(each_block)
-        #     each_block = []
-        #     beep.append(block)
-
-        #     counter = counter + 1
-        #     block = {}
-        #     block['text'] = []
-        #     block['desc'] = ''
-        #     block['title'] = ''
-        #     block["photos"] = []
-
-        # elif block_in_progress:
-        #     each_block.append(line)
-    # Finished scanning through ALL the messages.Return the list of blocks
-    # print(all_blocks)
-    print(beep)
     return all_blocks
 
 
 def save_products(all_blocks):
 
-    titles = Product.objects.values_list('title', flat=True)
-    # Each block represents a potential product
-    for each_block in all_blocks:
-        text = ""
-        photos = []
-        for item in each_block:
-            if item.startswith(base_url) and (bot_token in item) and ("photos" in item):
-                photos.append(item)  # most likely a photo file location
-            else:
-                # At the time of writing, only text and photos are supported
-                # So, if it is not a photo, its a 'text' - concatenate.
-                text += item
-
-        # Form a PRODUCT Object
-        get_product_title_desc_res = get_product_title_desc(text)
-        title = get_product_title_desc_res["title"]
-        description = get_product_title_desc_res["desc"]
-        print("Creating product with title[{}]..".format(title))
-        if (title in titles):
-            # item already exists. Skip.
-            print("Creating product with title[{}]..SKIPPED!".format(title))
-            print("Item already exists!")
-            continue
-        if(len(photos) == 0):
+    # Each block represents a new product
+    for block in all_blocks:
+        title = block["title"]
+        desc = block["desc"]
+        category_id = block["category_id"]
+        # Handle photos
+        photos = block["photos"]
+        if len(photos) == 0:
             photo_url = default_photo_url
         else:
-            photo_url = photos.pop(0)
-        product = Product(
-            title=title,
-            image=get_photo_from_url(photo_url),
-            category=Category.objects.get(id=15),
-            description=description
-        )
+            photo_id = photos.pop(0)
+            photo_url = get_photo_url_from_id(photo_id)
+        photo_obj = get_photo_from_url(photo_url)
+
+        print("Creating product with title[{}]..".format(title))
+        product = Product()
+        product.title = title
+        product.description = desc
+        product.category = Category.objects.get(id=category_id)
+        product.image.save("logo.png", photo_obj, save=False)
         product.save()
-        print("Creating product with title[{}]..Done [productid={}]".format(
-            title, product.id))
+        print(
+            "Creating product with title[{}]..Done [productid={}]".format(
+                title, product.id
+            )
+        )
         print("Adding additional photos for product(if any)..")
-        for photo_url in photos:
-            productimage = ProductImage(
-                product=product,
-                image=get_photo_from_url(photo_url))
+        for photo_id in photos:
+            photo_url = get_photo_url_from_id(photo_id)
+            photo_obj = get_photo_from_url(photo_url)
+            productimage = ProductImage(product=product, image=photo_obj)
             productimage.save()
         print("Adding additional photos for product(if any)..Done")
         print("--")
 
 
-def get_product_title_desc(text):
+def compute_title_desc(all_blocks):
 
     #   - PRODUCT has a title and description.
     #   - Attempt to extract the title
     #       - The line generally looks like: Catalog Name: Title goes here
     #       - In few cases, the "Catalog Name" is not present.
     #           - In such cases fall back to use the first line as title
-    desc = []
-    title = ""
-    for line in text.splitlines():
-        if re.search("Catalog Name", line, re.IGNORECASE):
-            # We found the line. Ex. Catalog Name: blah blah blah
-            # Strip off the 'Catalog Name' so that we end up with 'blah blah blah' as title
-            # Using regex substitution to do so - substitute 'Catalog Name' with ''
-            title = re.sub("Catalog\s*Name:?", "", line, flags=re.IGNORECASE)
-            title = re.sub("^\*", "", title)
-            title = re.sub("\s*\*\s*$", "", title)
-        else:
-            desc.append(line)
+    for block in all_blocks:
+        text = block["text"]
+        merged_text = "\n".join(text)
+        desc = []
+        title = ""
+        category_id = ""
+        for line in merged_text.splitlines():
+            if re.search("Catalog Name", line, re.IGNORECASE):
+                # We found the line. Ex. Catalog Name: blah blah blah
+                # Strip off the 'Catalog Name' so that we end up with 'blah blah blah' as title
+                # Using regex substitution to do so - substitute 'Catalog Name' with ''
+                title = re.sub(r"Catalog\s*Name:?", "",
+                               line, flags=re.IGNORECASE)
+                title = re.sub(r"^\*", "", title)
+                title = re.sub(r"\s*\*\s*$", "", title)
+            if re.search(r"^\s*Category\s*=\s*\d+\s*$", line, re.IGNORECASE):
+                category_id = re.search(r"^\s*Category\s*=\s*(\d+)\s*$",
+                                        line, re.IGNORECASE).group(1)
+            else:
+                desc.append(line)
+        if title == "":
+            # It means there is NO line matching 'Catalog Name'.
+            # In such csaes, use the first line as title
+            title = desc.pop(0)
+        if category_id == "":
+            # It means there is NO line matching 'Category=100'
+            # In such csaes, use the default category id
+            category_id = default_category_id
 
-    if title == "":
-        # It means there is NO line matching 'Catalog Name'.
-        # In such csaes, use the first line as title
-        title = desc.pop(0)
-    desc = "\n".join(desc)
-    return {"title": title, "desc": desc}
+        desc = "\n".join(desc)
+        block["title"] = title
+        block["desc"] = desc
+        block["category_id"] = category_id
+        # We have now extracted the 'title'/'desc'/'category' from 'text' values.
+        # We dont need 'text' anymore. Delete it.
+        del block["text"]
+    # Done
+    return all_blocks
 
 
 def get_photo_id(message):
@@ -208,16 +190,22 @@ def get_photo_id(message):
         if photo_file["file_size"] > file_size:
             file_size = photo_file["file_size"]
             file_id = photo_file["file_id"]
+    return file_id
+
+
+def get_photo_url_from_id(photo_id):
+
     # Fetch the relatve file path from the fileid fetched above
     # https://api.telegram.org/bot<secretid>/getFile?file_id=<fileid>
-    # url = "https://api.telegram.org/bot{}/getFile?file_id={}".format(
-    #     bot_token, file_id)
-    # file_path = requests.get(url).json()["result"]["file_path"]
+    url = "https://api.telegram.org/bot{}/getFile?file_id={}".format(
+        bot_token, photo_id
+    )
+    file_path = requests.get(url).json()["result"]["file_path"]
     # Form the "fully qualified" path to the photo file
     # https://api.telegram.org/file/bot<secretid>/<file_path>
-    # photo_url = "https://api.telegram.org/file/bot{}/{}".format(
-    #     bot_token, file_path)
-    return file_id
+    photo_url = "https://api.telegram.org/file/bot{}/{}".format(
+        bot_token, file_path)
+    return photo_url
 
 
 def get_photo_from_url(photo_url):
@@ -233,10 +221,30 @@ def get_photo_from_url(photo_url):
 
 
 def get_categories():
-    print("get categories")
-    categories = Category.objects.all()
-    print(categories)
+    text = "Select category.\nReply like Category=10\n"
+    categories = Category.objects.all().order_by("id")
     for category in categories:
-        print(category.id)
-        print(category.sequence)
-        print(category.name)
+        text += "{}: {}\n".format(category.id, category.name)
+    return text
+
+
+def remove_existing_products(all_blocks):
+    titles = Product.objects.values_list("title", flat=True)
+    filtered_blocks = []
+    for block in all_blocks:
+        title = block["title"]
+        if title.upper() in (x.upper() for x in titles):
+            print(
+                "Product with title[{}] already exists!Skipping.".format(title))
+        else:
+            filtered_blocks.append(block)
+    return filtered_blocks
+
+
+def send_message(chat_id, message):
+    """Bot sends message to user
+    https://api.telegram.org/bot{bot-toke}/sendMessage?chat_id={chat-id}&text={message}
+    """
+    url = "https://api.telegram.org/bot{}/sendMessage".format(bot_token)
+    # myobj = {"chat_id": chat_id, "text": message}
+    requests.post(url, data={"chat_id": chat_id, "text": message})
