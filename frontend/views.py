085c98b0d43dede794ae37ec3ac448afc155bea4
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-08-01 04:15:42 +0530

    .

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,6 @@
 import json
 import time
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.core import serializers
 from django.shortcuts import render, get_object_or_404
 
@@ -16,19 +16,12 @@ def index(request):
 
 def product_view(request, categoryname):
     start = time.time()
-    print(time.time())
     subcategories = list(SubCategory.objects.filter(category__name=categoryname))
-    print(">>>")
-    print(subcategories)
-    print("<<<")
-    print(time.time())
     products = {}
     for subcategory in subcategories:
-        print(time.time())
         subcategory_name = subcategory.name
         products[subcategory_name] = {}
         prds = list(Product.objects.filter(subcategory=subcategory).order_by("id"))
-        # prds = Product.objects.filter(subcategory=subcategory).order_by("id")
         for product in prds:
             product_id = product.id
             products[subcategory_name][product_id] = {}
@@ -37,35 +30,11 @@ def product_view(request, categoryname):
             products[subcategory_name][product_id]["description"] = product.description
             products[subcategory_name][product_id]["images"] = []
             products[subcategory_name][product_id]["images"].append(product.image.url)
-            photos_qs = list(ProductImage.objects.filter(product=product))
-            for photo in photos_qs:
+            photos = list(ProductImage.objects.filter(product=product))
+            for photo in photos:
                 products[subcategory_name][product_id]["images"].append(photo.image.url)
 
-    print(products)
-    end = time.time()
-    print(end - start)
-    # for subcategory in products:
-    #     print(subcategory)
-    #     for product in products[subcategory]:
-    #         print("--")
-    #         print(product.title)
-
-    return HttpResponse(end - start)
-    # for product in products:
-    #     products_details[product.id] = {}
-    #     products_details[product.id]["sku"] = product.sku
-    #     products_details[product.id]["title"] = product.title
-    #     products_details[product.id]["subcategory"] = product.subcategory
-    #     products_details[product.id]["description"] = product.description
-    #     products_details[product.id]["images"] = []
-    #     products_details[product.id]["images"].append(product.image.url)
-
-    #     photos_qs = ProductImage.objects.filter(product=product)
-    #     for photo in photos_qs:
-    #         products_details[product.id]["images"].append(photo.image.url)
-    # return render(
-    #     request, "frontend/products.html", {"products_details": products_details},
-    # )
+    return render(request, "frontend/products.html", {"products_details": products},)
 
 
 def detail_view(request, id):
